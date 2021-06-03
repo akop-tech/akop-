ce7c92b87cf9262e2f2c2812d2c82fb4bb2e9316
AbdullahMamdou1 <>
2021-06-03 17:27:27 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  char username; 
+  printf("Enter you user name: ");
+  scanf("akop");
+  printf(" wellcome  AkopGaming \n ");
+  char password ;
+  printf("Enter you password : ");
+  scanf("%c", & password );
+  printf("Wellcome \n ");
+  printf("copy this link \n");
+  printf("")
+  return 0;
+}
